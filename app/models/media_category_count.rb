04799ddaed40c8b8c3b96610afb97f4a8d8124fe
Andres Montano <amontano@virginia.edu>
2010-02-15 10:19:06 -0600

    Header can't be nil. Better to not set at all in that case.

--- app/models/media_category_count.rb
+++ app/models/media_category_count.rb
@@ -1,5 +1,5 @@
 class MediaCategoryCount < MediaManagementResource
-  headers['Host'] = MediaManagementResource.headers['Host']
+  headers['Host'] = MediaManagementResource.headers['Host'] if !MediaManagementResource.headers['Host'].blank?
   self.site = "#{self.site.to_s}categories/:category_id/"
   self.element_name = 'count'
 end
