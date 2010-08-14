aca7a93499f244edce783ec054abdf97a1f72861
Andres Montano <amontano@virginia.edu>
2010-08-14 15:14:03 -0600

    validating errors in api

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -3,18 +3,26 @@ class Medium < MediaManagementResource
   self.element_name = 'media_object'
   
   def category_ids
-    if self.associated_category.instance_of? Array
-      return self.associated_category.collect{|c| c.id.to_i }
-    else
-      return [self.associated_category.id.to_i]
+    begin
+      if self.associated_category.instance_of? Array
+        return self.associated_category.collect{|c| c.id.to_i }
+      else
+        return [self.associated_category.id.to_i]
+      end
+    rescue
+      return []
     end
   end
 
   def feature_ids
-    if self.associated_feature.instance_of? Array
-      return self.associated_feature.collect{|c| c.fid.to_i }
-    else
-      return [self.associated_feature.fid.to_i]
+    begin
+      if self.associated_feature.instance_of? Array
+        return self.associated_feature.collect{|c| c.fid.to_i }
+      else
+        return [self.associated_feature.fid.to_i]
+      end
+    rescue
+      return []
     end
   end
 end
