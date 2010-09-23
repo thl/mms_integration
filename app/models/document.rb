d3d327e9b53381aa5ab9234a209e2aa8820bd937
Andres Montano <amontano@virginia.edu>
2010-09-23 15:54:14 -0600

    compensating for no title

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,13 +2,10 @@ class Document < MediaManagementResource
   headers['Host'] = MediaManagementResource.headers['Host'] if !MediaManagementResource.headers['Host'].blank?
   
   def prioritized_title
-    titles = self.title
-    if titles.instance_of?(Array)
-      title = titles.first
-    else
-      title = titles
-    end
-    title.title
+    titles = self.attributes['title']
+    return self.id if titles.nil?
+    return titles.first.title if titles.instance_of?(Array)
+    return titles.title
   end
   
   def self.find_by_title(title)
