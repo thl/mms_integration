7cafd52c7bc9600d7b1bc7787f50b5fc42b09b20
Andres Montano <amontano@virginia.edu>
2013-03-29 17:10:25 -0600

    handling oddities on active resource behavior due to caching

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,11 +1,16 @@
 class Document < MediaManagementResource
   headers['Host'] = MediaManagementResource.headers['Host'] if !MediaManagementResource.headers['Host'].blank?
   
+  # Using nested active resource. This is problematic when caching
   def prioritized_title
     titles = self.attributes['title']
     return self.id if titles.nil?
-    return titles.first.title if titles.instance_of?(Array)
-    return titles.title
+    if titles.instance_of?(Array)
+      elem = titles.first
+    else
+      elem = titles
+    end
+    return elem.instance_of?(Hash) ? elem['title'] : elem.title 
   end
   
   def self.find_by_title(title)
--- app/models/medium.rb
+++ app/models/medium.rb
@@ -2,24 +2,26 @@ class Medium < MediaManagementResource
   headers['Host'] = MediaManagementResource.headers['Host'] if !MediaManagementResource.headers['Host'].blank?
   self.element_name = 'media_object'
   
+  # Using nested active resource. This is problematic when caching
   def category_ids
     begin
       if self.associated_category.instance_of? Array
-        return self.associated_category.collect{|c| c.id.to_i }
+        return self.associated_category.collect{|c| (c.instance_of?(Hash) ? c['id'] : c.id).to_i }
       else
-        return [self.associated_category.id.to_i]
+        return [(self.associated_category.instance_of?(Hash) ? self.associated_category['id'] : self.associated_category.id).to_i]
       end
     rescue
       return []
     end
   end
 
+  # Using nested active resource. This is problematic when caching
   def feature_ids
     begin
       if self.associated_feature.instance_of? Array
-        return self.associated_feature.collect{|c| c.fid.to_i }
+        return self.associated_feature.collect{|c| (c.instance_of?(Hash) ? c['fid'] : c.fid).to_i }
       else
-        return [self.associated_feature.fid.to_i]
+        return [(self.associated_feature.instance_of?(Hash) ? self.associated_feature['fid'] : self.associated_feature.fid).to_i]
       end
     rescue
       return []
