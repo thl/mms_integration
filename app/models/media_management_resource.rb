556f4318617b2bd52080a56872ee6f23185e92df
Andres Montano <amontano@virginia.edu>
2013-05-17 11:55:26 -0600

    using interface utils for server recognition

--- app/models/media_management_resource.rb
+++ app/models/media_management_resource.rb
@@ -1,21 +1,22 @@
 class MediaManagementResource < ActiveResource::Base
-  hostname = Socket.gethostname.downcase
-  if hostname == 'sds6.itc.virginia.edu'
-    self.site = 'http://127.0.0.1/'
-    headers['Host'] = 'staging.mms.thlib.org'
-  elsif hostname == 'dev.thlib.org'
+  case InterfaceUtils::Server.environment
+  when InterfaceUtils::Server::DEVELOPMENT
     self.site = 'http://127.0.0.1/'
     headers['Host'] = 'dev-mms.thlib.org'
-  elsif hostname == 'e-bhutan.bt'
-    self.site = 'http://www.e-bhutan.net.bt/mms/'
-  elsif hostname.ends_with?('local') || hostname.starts_with?('vpn-user')
-    self.site = 'http://localhost/mms/'
-  elsif hostname =~ /sds.+\.itc\.virginia\.edu/
+  when InterfaceUtils::Server::STAGING
+    self.site = 'http://127.0.0.1/'
+    headers['Host'] = 'staging.mms.thlib.org'
+  when InterfaceUtils::Server::PRODUCTION
     self.site = 'http://127.0.0.1/'
     headers['Host'] = 'mms.thlib.org'
+  when InterfaceUtils::Server::LOCAL
+    self.site = 'http://localhost/mms/'
+  when InterfaceUtils::Server::EBHUTAN
+    self.site = 'http://www.e-bhutan.net.bt/mms/'
   else
     self.site = 'http://mms.thlib.org/'
-  end  
+  end
+
   self.timeout = 100
   self.format = :xml
 end
--- lib/mms_integration/version.rb
+++ lib/mms_integration/version.rb
@@ -1,3 +1,3 @@
 module MmsIntegration
-  VERSION = '2.0.4'
+  VERSION = '2.0.5'
 end
