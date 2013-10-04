4d6eab4e381e07556bbf87ac4be6534a463991ea
Andres Montano <amontano@virginia.edu>
2013-10-03 23:10:43 -0600

    adding width and height to image

--- app/models/mms_integration/picture.rb
+++ app/models/mms_integration/picture.rb
@@ -2,14 +2,31 @@ module MmsIntegration
   class Picture < MediaManagementResource
     headers['Host'] = MediaManagementResource.headers['Host'] if !MediaManagementResource.headers['Host'].blank?
     
+    def image
+      @image ||= self.images.detect{|i| i.thumbnail == default_thumbnail}
+    end
+    
     def url
-      i = self.images.detect{|i| i.thumbnail=='essay'}
-      i.url if !i.nil?
+      image.url if !image.nil?
+    end
+    
+    def width
+      @width ||= image.width.to_i if !image.nil?
+    end
+    
+    def height
+      @height ||= image.height.to_i if !image.nil?
     end
     
     def caption
       c = self.captions.first
       c.title if !c.nil?
+    end
+    
+    private
+    
+    def default_thumbnail
+      'essay'
     end
   end
 end
