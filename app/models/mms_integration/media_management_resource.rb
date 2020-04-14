d7a5fad647af37a2ad8af4fb3d98cc24da84b123
Andres Montano <amontano@virginia.edu>
2020-04-14 13:47:49 -0600

    getting rid ebhutan

--- app/models/mms_integration/media_management_resource.rb
+++ app/models/mms_integration/media_management_resource.rb
@@ -2,18 +2,13 @@ module MmsIntegration
   class MediaManagementResource < ActiveResource::Base
     case InterfaceUtils::Server.environment
     when InterfaceUtils::Server::DEVELOPMENT
-      self.site = 'http://127.0.0.1/'
-      headers['Host'] = 'dev-mms.thlib.org'
+      self.site = 'dev-mms.thlib.org'
     when InterfaceUtils::Server::STAGING
-      self.site = 'http://127.0.0.1/'
-      headers['Host'] = 'staging-mms.thlib.org'
+      self.site = 'staging-mms.thlib.org'
     when InterfaceUtils::Server::PRODUCTION
-      self.site = 'http://127.0.0.1/'
-      headers['Host'] = 'mms.thlib.org'
+      self.site = 'mms.thlib.org'
     when InterfaceUtils::Server::LOCAL
       self.site = 'http://localhost/mms/'
-    when InterfaceUtils::Server::EBHUTAN
-      self.site = 'http://www.e-bhutan.net.bt/mms/'
     else
       self.site = 'http://mms.thlib.org/'
     end
--- lib/mms_integration/version.rb
+++ lib/mms_integration/version.rb
@@ -1,3 +1,3 @@
 module MmsIntegration
-  VERSION = '2.3.6'
+  VERSION = '2.3.8'
 end
