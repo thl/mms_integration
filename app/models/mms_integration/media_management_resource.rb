79a0d9b251fa917d2219b8cd955e88d740fa34da
Andres Montano <amontano@virginia.edu>
2020-04-29 22:25:53 -0600

    adding http to urls

--- app/models/mms_integration/media_management_resource.rb
+++ app/models/mms_integration/media_management_resource.rb
@@ -2,11 +2,11 @@ module MmsIntegration
   class MediaManagementResource < ActiveResource::Base
     case InterfaceUtils::Server.environment
     when InterfaceUtils::Server::DEVELOPMENT
-      self.site = 'dev-mms.thlib.org'
+      self.site = 'http://dev-mms.thlib.org'
     when InterfaceUtils::Server::STAGING
-      self.site = 'staging-mms.thlib.org'
+      self.site = 'http://staging-mms.thlib.org'
     when InterfaceUtils::Server::PRODUCTION
-      self.site = 'mms.thlib.org'
+      self.site = 'http://mms.thlib.org'
     when InterfaceUtils::Server::LOCAL
       self.site = 'http://localhost/mms/'
     else
