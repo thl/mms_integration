f266594723ebb6410953d6757ff2ad6fc96ec6cf
Andres Montano <amontano@virginia.edu>
2020-06-01 10:14:02 -0600

    getting rid of sds specific code

--- app/models/mms_integration/media_category_count.rb
+++ app/models/mms_integration/media_category_count.rb
@@ -1,6 +1,5 @@
 module MmsIntegration
   class MediaCategoryCount < ActiveResource::Base
-    headers['Host'] = MediaManagementResource.headers['Host'] if !MediaManagementResource.headers['Host'].blank?
     self.site = "#{MediaManagementResource.site.to_s}categories/:category_id/"
     self.element_name = 'count'
     self.format = :xml
--- app/models/mms_integration/media_place_count.rb
+++ app/models/mms_integration/media_place_count.rb
@@ -1,6 +1,5 @@
 module MmsIntegration
   class MediaPlaceCount < ActiveResource::Base
-    headers['Host'] = MediaManagementResource.headers['Host'] if !MediaManagementResource.headers['Host'].blank?
     self.site = "#{MediaManagementResource.site.to_s}places/:place_id/"
     self.element_name = 'count'
     self.format = :xml
