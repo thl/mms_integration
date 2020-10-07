3925e8c2f3eb41fe8c1dcd855f2039f2d0733f71
Andres Montano <amontano@virginia.edu>
2020-10-07 16:48:25 -0500

    undoing unnecessary changes

--- app/models/mms_integration/media_management_resource.rb
+++ app/models/mms_integration/media_management_resource.rb
@@ -1,6 +1,12 @@
 module MmsIntegration
   class MediaManagementResource < ActiveResource::Base
     case InterfaceUtils::Server.environment
+    when InterfaceUtils::Server::DEVELOPMENT
+      self.site = 'http://dev-mms.thlib.org/'
+    when InterfaceUtils::Server::STAGING
+      self.site = 'http://staging-mms.thlib.org/'
+    when InterfaceUtils::Server::PRODUCTION
+      self.site = 'http://mms.thlib.org/'
     when InterfaceUtils::Server::LOCAL
       self.site = 'http://localhost/mms/'
     else
--- lib/mms_integration/version.rb
+++ lib/mms_integration/version.rb
@@ -1,3 +1,3 @@
 module MmsIntegration
-  VERSION = '2.4.3'
+  VERSION = '2.4.2'
 end
